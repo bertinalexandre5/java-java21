3ffd72e2e958c7245d1680410976e9491f2ef28f
Alexandre Bertin <bertin.alexandre5@gmail.com>
2026-07-24 12:35:08 +0200

    Tp 4 ex1, 2, 3 ok

--- src/test/java/java17/ex03/Stream_03_Test.java
+++ src/test/java/java17/ex03/Stream_03_Test.java
@@ -24,87 +24,95 @@ import static org.junit.Assert.*;
  */
 public class Stream_03_Test {
 
-	@Test
-	public void test_max() throws Exception {
+    @Test
+    public void test_max() throws Exception {
 
-		List<Order> orders = new Data().getOrders();
+        List<Order> orders = new Data().getOrders();
 
-		// TODO Retrouver la commande avec le prix le plus élevé
-		Optional<Order> result = null;
+        // TODO Retrouver la commande avec le prix le plus élevé
+        Optional<Order> result = orders.stream()
+                .max(Comparator.comparingDouble(order -> order.getPrice()));
 
-		assertThat(result.isPresent(), is(true));
-		assertThat(result.get().getPrice(), is(2200.0));
-	}
+        assertThat(result.isPresent(), is(true));
+        assertThat(result.get().getPrice(), is(2200.0));
+    }
 
-	@Test
-	public void test_min() throws Exception {
+    @Test
+    public void test_min() throws Exception {
 
-		List<Order> orders = new Data().getOrders();
+        List<Order> orders = new Data().getOrders();
 
-		// TODO Retrouver la commande avec le prix le moins élevé
-		Optional<Order> result = null;
+        // TODO Retrouver la commande avec le prix le moins élevé
+        Optional<Order> result = orders.stream()
+                .min(Comparator.comparingDouble(order -> order.getPrice()));
+        ;
 
-		assertThat(result.isPresent(), is(true));
-		assertThat(result.get().getPrice(), is(1000.0));
-	}
+        assertThat(result.isPresent(), is(true));
+        assertThat(result.get().getPrice(), is(1000.0));
+    }
 
-	@Test
-	public void test_map_collect_joining() throws Exception {
+    @Test
+    public void test_map_collect_joining() throws Exception {
 
-		List<Customer> customers = new Data().getCustomers();
+        List<Customer> customers = new Data().getCustomers();
 
-		// TODO construire une chaîne contenant les prénoms des clients triés et séparés
-		// par le caractère "|"
-		String result = null;
+        // TODO construire une chaîne contenant les prénoms des clients triés et séparés
+        // par le caractère "|"
+        String result = customers.stream()
+                //.collect(Collectors.groupingBy(o -> o.getFirstname()));
+                .map(customer -> customer.getFirstname())
+                .sorted()
+                .collect(Collectors.joining("|"));
 
-		assertThat(result, is("Alexandra|Cyril|Johnny|Marion|Sophie"));
-	}
+        assertThat(result, is("Alexandra|Cyril|Johnny|Marion|Sophie"));
+    }
 
-	@Test
-	public void test_flatMap() throws Exception {
+    @Test
+    public void test_flatMap() throws Exception {
 
-		List<Order> orders = new Data().getOrders();
+        List<Order> orders = new Data().getOrders();
 
-		// TODO Extraire la liste des pizzas de toutes les commandes
-		List<Pizza> result = null;
+        // TODO Extraire la liste des pizzas de toutes les commandes
+        List<Pizza> result = orders.stream()
+                ;
 
-		assertThat(result.size(), is(9));
-	}
+        assertThat(result.size(), is(9));
+    }
 
-	@Test
-	public void test_flatMap_distinct() throws Exception {
+    @Test
+    public void test_flatMap_distinct() throws Exception {
 
-		List<Order> orders = new Data().getOrders();
+        List<Order> orders = new Data().getOrders();
 
-		// TODO Extraire la liste des différentes pizzas de toutes les commandes
-		List<Pizza> result = null;
+        // TODO Extraire la liste des différentes pizzas de toutes les commandes
+        List<Pizza> result = null;
 
-		assertThat(result.size(), is(4));
-	}
+        assertThat(result.size(), is(4));
+    }
 
-	@Test
-	public void test_grouping() throws Exception {
+    @Test
+    public void test_grouping() throws Exception {
 
-		List<Order> orders = new Data().getOrders();
+        List<Order> orders = new Data().getOrders();
 
-		// TODO construire une Map <Client, Commandes effectuées par le client
-		Map<Customer, List<Order>> result = null;
+        // TODO construire une Map <Client, Commandes effectuées par le client
+        Map<Customer, List<Order>> result = null;
 
-		assertThat(result.size(), is(2));
-		assertThat(result.get(new Customer(1)), hasSize(4));
-		assertThat(result.get(new Customer(2)), hasSize(4));
-	}
+        assertThat(result.size(), is(2));
+        assertThat(result.get(new Customer(1)), hasSize(4));
+        assertThat(result.get(new Customer(2)), hasSize(4));
+    }
 
-	@Test
-	public void test_partitionning() throws Exception {
-		List<Pizza> pizzas = new Data().getPizzas();
+    @Test
+    public void test_partitionning() throws Exception {
+        List<Pizza> pizzas = new Data().getPizzas();
 
-		// TODO Séparer la liste des pizzas en 2 ensembles :
-		// TODO true -> les pizzas dont le nom commence par "L"
-		// TODO false -> les autres
-		Map<Boolean, List<Pizza>> result = null;
+        // TODO Séparer la liste des pizzas en 2 ensembles :
+        // TODO true -> les pizzas dont le nom commence par "L"
+        // TODO false -> les autres
+        Map<Boolean, List<Pizza>> result = null;
 
-		assertThat(result.get(true), hasSize(6));
-		assertThat(result.get(false), hasSize(2));
-	}
+        assertThat(result.get(true), hasSize(6));
+        assertThat(result.get(false), hasSize(2));
+    }
 }
